ad54dc11120b41bd76996803dd4f93faf7ae2624
Oboe + Werkstatt <kontakt@oboe-werkstatt.de>
2025-12-13 08:43:53 +0100

    requestAnimationFrame()

--- models/world.class.js
+++ models/world.class.js
@@ -7,13 +7,29 @@ class World {
         new Chicken(),
     ];
 
-    constructor(canvas){
+    canvas;
+    ctx;
+
+    constructor(canvas) {
         this.ctx = canvas.getContext('2d'); // ctx = canvas.getContext('2d');
         this.draw();
     }
 
 
+    // Call draw over and over again
     draw() {
-        this.ctx.drawImage(this.character.img,this.character.x,this.character.y, this.character.height, this.character.width);
+
+        this.ctx.clearRect(0,0, this.canvas.width, this.canvas.height);
+
+        this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.height, this.character.width);
+        this.enemies.forEach(enemy => {
+            this.ctx.drawImage(enemy.img,enemy.x,enemy.y,enemy.width,enemy.height);
+        });
+
+        let self = this;
+        requestAnimationFrame(function () {
+            self.draw();
+        });
     }
+
 }
