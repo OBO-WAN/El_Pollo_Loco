456b3dab2894dd6b38b2c359701473cb05504624
Oboe + Werkstatt <kontakt@oboe-werkstatt.de>
2025-12-20 12:27:35 +0100

    loadMainCharacter(),ready to be animated

--- models/character.class.js
+++ models/character.class.js
@@ -1,14 +1,23 @@
 class Character extends movableObject{
 
     height = 240;
-    width = 60;
+    width = 140;
     x = 120;
     y = 200;
     
 
     constructor(){
         super().loadImage('img/2_character_pepe/2_walk/W-21.png');
-        this.width = 180;
+        this.loadMainCharacter([
+        'img/2_character_pepe/2_walk/W-21.png',
+        'img/2_character_pepe/2_walk/W-22.png',
+        'img/2_character_pepe/2_walk/W-23.png',
+        'img/2_character_pepe/2_walk/W-24.png',
+        'img/2_character_pepe/2_walk/W-25.png',
+        'img/2_character_pepe/2_walk/W-26.png'  
+    
+    ]);
+        
     }
 
     jump(){
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,14 +1,27 @@
 class movableObject {
     x = 120;
     y = 360;
-    img ;
+    img;
     height = 100;
     width = 50;
+    imageCache = {};
 
 
-    loadImage(path){
+    loadImage(path) {
         this.img = new Image(); // im Grunde das gleiche wie document.getElementById("");
         this.img.src = path;
+    }
+
+    /**
+     * 
+     * @param {Array} arr -[images from imageCache] 
+     */
+    loadMainCharacter(arr) {
+        arr.forEach((path) => {
+            let img = new Image();
+            img.src = path;
+            this.imageCache = path;
+        });
     }
 
     moveRight() {
