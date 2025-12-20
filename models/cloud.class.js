cfcef3eecd12da1e7173a7c861c27343df9db55c
Oboe + Werkstatt <kontakt@oboe-werkstatt.de>
2025-12-20 09:55:10 +0100

    cloud movement added/cloud.class.js

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -1,18 +1,20 @@
-class Cloud extends movableObject{
+class Cloud extends movableObject {
 
     y = 20;
-    height = 250; 
+    height = 250;
 
- 
-       constructor(){
+
+    constructor() {
         super().loadImage('img/5_background/layers/4_clouds/1.png');
-            this.x = Math.random() * 500;
-            // this.y = 50;
-            this.width = 490;
-            // this.height = 150;
-
-
+        this.x = Math.random() * 500;
+        this.width = 490;
+        this.animateClouds();
     }
 
+    animateClouds() {
+        setInterval(() => {
+            this.x -= 0.15; // 0.15 px pro Berechnung werden hier abgezogen
+        },1000 / 60);
+    }
 
 }
