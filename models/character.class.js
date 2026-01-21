25b8f0030865d14ae53a4c9f80017b1f86310c8d
Oboe + Werkstatt <kontakt@oboe-werkstatt.de>
2026-01-21 17:36:16 +0100

    Gravity images added

--- models/character.class.js
+++ models/character.class.js
@@ -4,7 +4,7 @@ class Character extends movableObject {
     width = 140;
     x = 120;
     y = 40; // 40-200
-    speed = 6;
+    speed = 10;
 
     animatedCharachter = [
         'img/2_character_pepe/2_walk/W-21.png',
@@ -16,6 +16,19 @@ class Character extends movableObject {
 
     ];
 
+    imagesJumping = [
+
+        'img/2_character_pepe/3_jump/J-31.png',
+        'img/2_character_pepe/3_jump/J-32.png',
+        'img/2_character_pepe/3_jump/J-33.png',
+        'img/2_character_pepe/3_jump/J-34.png',
+        'img/2_character_pepe/3_jump/J-35.png',
+        'img/2_character_pepe/3_jump/J-36.png',
+        'img/2_character_pepe/3_jump/J-37.png',
+        'img/2_character_pepe/3_jump/J-38.png',
+        'img/2_character_pepe/3_jump/J-39.png',
+    ]
+
     world;
 
     // currentImage = 0;
@@ -25,15 +38,26 @@ class Character extends movableObject {
         super();
         this.loadImage('img/2_character_pepe/2_walk/W-21.png');
         this.loadImages(this.animatedCharachter);
+        this.loadImages(this.imagesJumping);
         this.applyGravity();
         this.animate();
     }
 
     animate() {
-    setInterval(() => {
-        this.playAnimation(this.animatedCharachter);
-    }, 100);
-}
+        setInterval(() => {
+
+            if (this.isAboveGround) {
+                this.playAnimation(this.imagesJumping);
+
+            } else {
+                if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+                    this.playAnimation(this.animatedCharachter);
+                }
+            }
+
+        }, 100);
+
+    }
 
     jump() {
 
