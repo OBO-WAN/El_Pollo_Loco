a9c74bde9ca94932527386b02051d4f89a2ee61f
Oboe + Werkstatt <kontakt@oboe-werkstatt.de>
2026-02-03 14:57:24 +0100

    css improvements + start screen (Overlay)

--- js/game.js
+++ js/game.js
@@ -1,32 +1,40 @@
 let canvas;
 let world;
-
 let keyboard = new Keyboard();
 
 
 function init() {
-    canvas = document.getElementById('canvas');
-    world = new World(canvas, keyboard);
-
-    // console.log("My character is: ", world.character);
-
+  canvas = document.getElementById('canvas');
+  document.getElementById('startBtn').addEventListener('click', startGame);
+  window.addEventListener('keydown', (e) => {
+    if (e.code === 'Enter') startGame();
+  });
 }
 
+function startGame() {
+  if (world) return; // already started
+  document.getElementById('startScreen').style.display = 'none';
+  world = new World(canvas, keyboard);
+}
+
+
 window.addEventListener('keydown', (e) => {
+  if (['ArrowRight', 'ArrowLeft', 'ArrowUp', 'Space'].includes(e.code)) e.preventDefault(); // prevents scrolling
+
   if (e.code === 'ArrowRight') keyboard.RIGHT = true;
-  if (e.code === 'ArrowLeft')  keyboard.LEFT = true;
-  if (e.code === 'ArrowUp')    keyboard.UP = true;
-  if (e.code === 'Space')      keyboard.SPACE = true;
+  if (e.code === 'ArrowLeft') keyboard.LEFT = true;
+  if (e.code === 'ArrowUp') keyboard.UP = true;
+  if (e.code === 'Space') keyboard.SPACE = true;
 });
 
 window.addEventListener('keyup', (e) => {
+
+  if (['ArrowRight', 'ArrowLeft', 'ArrowUp', 'Space'].includes(e.code)) e.preventDefault(); // prevents scrolling
+
   if (e.code === 'ArrowRight') keyboard.RIGHT = false;
-  if (e.code === 'ArrowLeft')  keyboard.LEFT = false;
-  if (e.code === 'ArrowUp')    keyboard.UP = false;
-  if (e.code === 'Space')      keyboard.SPACE = false;
-
-  if (['ArrowRight','ArrowLeft','ArrowUp','Space'].includes(e.code)) e.preventDefault(); // prevents scrolling
-
+  if (e.code === 'ArrowLeft') keyboard.LEFT = false;
+  if (e.code === 'ArrowUp') keyboard.UP = false;
+  if (e.code === 'Space') keyboard.SPACE = false;
 });
 
 
